a37ebf0600fd2240e089ad19b2c4d1e32af3b508
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-08-10 18:08:21 -0400

    Change if after school.

--- public/jsc/application.js
+++ public/jsc/application.js
@@ -67,8 +67,29 @@ function clock(){
 	$('span#time').html(hour + ":" + minute + ":" + second + " " + am_pm)
 	
 	$(document).ready(function(){
-
+	
+	var periods = 0;
+	
 	$('.period').each(function() {
+		periods = periods + 1
+		
+	});
+	
+	$('.period').each(function() {
+		var element = $(this);
+		var finish = element.find('time.finish').attr('time');
+		
+		d = new Date();
+		var time = (d.getHours() * 3600) + (d.getMinutes() * 60)
+		
+		if (time > finish) {
+			$(".during").css("display", "none");
+			$(".after").css("display", "block");
+		}
+		
+	});
+	
+	$('.period').each(function(index) {
 		var element = $(this);
 		
 		var start = element.find('time.start').attr('time');
@@ -86,6 +107,8 @@ function clock(){
     	}
 
 	});
+	
+	
 
 });
 };
@@ -94,9 +117,3 @@ setInterval("clock()", 0);
 var periods = "/api/periods.json"
 var marquees = "/api/marquee.json"
 var notices = "/api/notices.json"
-
-$.getJSON(periods, function(json) {
-	$(json).each(function() {
-		
-	}
-});
