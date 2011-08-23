ea1ffac09537f2d70dde17fcb197708f96bd575d
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-08-23 15:36:21 -0400

    Now only 600px wide.

--- public/jsc/application.js
+++ public/jsc/application.js
@@ -134,7 +134,6 @@ setInterval("clock()", 0);
 	III. Reload
 */
 
-/*
 function refresh(hours, minutes, seconds) {
     var now = new Date();
     var then = new Date();
@@ -153,11 +152,7 @@ function refresh(hours, minutes, seconds) {
 }
 
 refresh(1,0,0);
-*/
 
-$(document).ready(function(){
-	$('input').attr('tabindex', '0').focus();
-});
 
 /*
 	V. API
