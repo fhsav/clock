66d44d02230a790f50c482ef9bc6e23fdb2ccfc3
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-09-07 12:51:31 -0400

    Added some non-passing test.

--- spec/admin/controllers/schedules.rb
+++ spec/admin/controllers/schedules.rb
@@ -16,4 +16,46 @@ end
 
 describe "Schedule edit page" do
 
+  before do
+    post "/admin/authenticate", :password => @@yaml["password"]
+    
+    let(:schedule) { Schedule.new(:name => "Foo") }
+  end
+  
+  it "should exist" do
+    get "/admin/schedules/edit/#{schedule.id}"
+    
+    last_response.should be_ok
+  end
+
+end
+
+describe "Schedule API" do
+  
+  before do
+    post "/admin/authenticate", :password => @@yaml["password"]
+  end
+  
+  it "creation should work" do
+    post "/admin/schedules/create", :schedule => { :name => "Foo", :description => "Bar" }
+    
+    last_response.should be_ok
+  end
+  
+  it "editing should work" do
+    let(:schedule) { Schedule.new(:name => "Foo") }
+    
+    put "/admin/schedules/modify", :schedule => { :name => "Foo", :description => "Bar" }
+    
+    last_response.should be_ok
+  end
+  
+  it "deletion should work" do
+    let(:schedule) { Schedule.new(:name => "Foo") }
+    
+    delete "/admin/schedules/destroy", :id => schedule.id
+    
+    last_response.should be_ok
+  end
+  
 end
