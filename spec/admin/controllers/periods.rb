8c020541870851db1faec26a7df9d224a43a3d0a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-09-08 11:12:12 -0400

    Removed some tests. Using non-minified jquery.marquee.js. Version bump to 0.1.3.

--- spec/admin/controllers/periods.rb
+++ spec/admin/controllers/periods.rb
@@ -1,31 +1,7 @@
 require 'spec_helper'
 
-describe "Schedule index" do
+describe "Period API" do
   
-  before do
-    post "/admin/authenticate", :password => @@yaml["password"]
-  end
-  
-  it "should exist" do
-    get "/admin/schedules"
-    
-    last_response.should be_ok
-  end
-  
-end
 
-describe "Schedule edit page" do
-
-  before do
-    post "/admin/authenticate", :password => @@yaml["password"]
-    
-    let(:schedule) { Schedule.new }
-  end
-  
-  it "should exist" do
-    get "/admin/schedules/edit/#{schedule.id}"
-    
-    last_response.should be_ok
-  end
 
 end
--- spec/admin/controllers/schedules.rb
+++ spec/admin/controllers/schedules.rb
@@ -2,60 +2,6 @@ require 'spec_helper'
 
 describe "Schedule index" do
   
-  before do
-    post "/admin/authenticate", :password => @@yaml["password"]
-  end
-  
-  it "should exist" do
-    get "/admin/schedules"
-    
-    last_response.should be_ok
-  end
-  
-end
 
-describe "Schedule edit page" do
-
-  before do
-    post "/admin/authenticate", :password => @@yaml["password"]
-    
-    let(:schedule) { Schedule.new(:name => "Foo") }
-  end
-  
-  it "should exist" do
-    get "/admin/schedules/edit/#{schedule.id}"
-    
-    last_response.should be_ok
-  end
-
-end
-
-describe "Schedule API" do
-  
-  before do
-    post "/admin/authenticate", :password => @@yaml["password"]
-  end
-  
-  it "creation should work" do
-    post "/admin/schedules/create", :schedule => { :name => "Foo", :description => "Bar" }
-    
-    last_response.should be_ok
-  end
-  
-  it "editing should work" do
-    let(:schedule) { Schedule.new(:name => "Foo") }
-    
-    put "/admin/schedules/modify", :schedule => { :name => "Foo", :description => "Bar" }
-    
-    last_response.should be_ok
-  end
-  
-  it "deletion should work" do
-    let(:schedule) { Schedule.new(:name => "Foo") }
-    
-    delete "/admin/schedules/destroy", :id => schedule.id
-    
-    last_response.should be_ok
-  end
   
 end
