243ddc235107b50a594acf20c3996da239c7f103
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-09-15 18:29:32 -0400

    Cleaned up /config.

--- config/schedule.rb
+++
@@ -1,7 +0,0 @@
-PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
-
-set :output, PADRINO_ROOT + '/log/cron.log'
-
-every 1.day, :at => "12:00 am" do
-  command "/var/www/fhsclock/script/production/update"
-end
--- config/test.rb
+++
@@ -1,8 +0,0 @@
-require 'yaml'
-
-directory = File.expand_path(File.dirname(__FILE__))
-yaml = YAML::load(File.open(File.join(directory, '..', 'config', 'settings.yml')))
-
-if yaml["password"]
-  puts yaml["password"]
-end
