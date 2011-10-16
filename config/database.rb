c2ac7b1bf483b3f3ca3299876735f823d654383e
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-10-16 19:31:18 -0400

    Heroku tweaks ported over. Not deploying until other computer is working. Now using "production" database instead of "development".

--- config/database.rb
+++ config/database.rb
@@ -1,11 +1,18 @@
-logger = Logger.new(PADRINO_ROOT + "/log/mongodb.log")
+if ENV['HEROKU']
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10015)
+  MongoMapper.database = 'fhsclock'
+  MongoMapper.database.authenticate('Timelord', 'cl*ck')
+else
+  logger = Logger.new(PADRINO_ROOT + "/log/mongodb.log")  
+  
+  MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
 
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-
-case Padrino.env
-  when :development then MongoMapper.database = 'clock_development'
-  when :production  then MongoMapper.database = 'clock_production'
-  when :test        then MongoMapper.database = 'clock_test'
+  case Padrino.env
+    when :development then MongoMapper.database = 'clock_development'
+    when :production  then MongoMapper.database = 'clock_production'
+    when :test        then MongoMapper.database = 'clock_test'
+  end
+  
+  MongoMapper.logger
 end
 
-MongoMapper.logger
