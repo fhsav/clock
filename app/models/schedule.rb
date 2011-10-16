820ce28529eb0cda2dc09598708e81484c148392
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-10-16 19:38:24 -0400

    Removed Schedule model callbacks.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -10,40 +10,6 @@ class Schedule
   
   # Associations
   many :periods
-  
-  # Callbacks
-  after_save :activate
-  
-  # Methods
-  def active?
-    if self.active == true
-      return true
-    else
-      return false
-    end
-  end
-  
-  def activate!(id)
-    others = Schedule.all
-    
-    others.each do |other|
-      other.active = false
-    end
-    
-    new = Schedule.find(id)
-    new.active = true
-  end
-  
-  # Callbacks, cont.
-  def activate
-    others = Schedule.all(:active => true)
-    
-    others.each do |other|
-      other.active = false
-    end
-    
-    self.active = true
-  end
 end
 
 class Period
