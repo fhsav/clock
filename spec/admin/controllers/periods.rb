34bbbb96e3a9b7c6ba7a597ebb15c8f00d747c85
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-10-26 21:14:05 -0400

    Oops, forgot to remove old specs.

--- spec/admin/controllers/periods.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Period API" do
-  before(:all) do
-    post "/admin/authenticate", :password => password
-    schedule = Schedule.create(:name => "Derp")
-  end
-  
-end
