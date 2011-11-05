ad6671acf7dc01ff5a29a824140dbfbb932c2e01
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-11-04 23:10:31 -0400

    All tests now pass.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,24 +6,28 @@ end
 
 Clock.helpers do
   def local(time)
-    time = time - 14400
-    
-    hour = time.strftime("%I")
-    minute = time.strftime("%M")
-    
-    if hour.to_s.initial == "0"
-      hour = hour.to_s.slice!(1)
+    if time
+      time = time - 14400
+      
+      hour = time.strftime("%I")
+      minute = time.strftime("%M")
+      
+      if hour.to_s.initial == "0"
+        hour = hour.to_s.slice!(1)
+      end
+      
+      "#{hour}:#{minute}"
     end
-    
-    "#{hour}:#{minute}"
   end
   
   def seconds(time)
-    time = time - 14400
-    
-    hours = time.hour * 3600
-    minutes = time.min * 60
-    
-    hours + minutes
+    if time
+      time = time - 14400
+      
+      hours = time.hour * 3600
+      minutes = time.min * 60
+      
+      hours + minutes
+    end
   end
 end
