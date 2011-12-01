633fbf365e65f832598098b30f41263d4bcc8a12
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-12-01 18:51:27 -0500

    Refresh tests.

--- spec/admin/controllers/core_spec.rb
+++ spec/admin/controllers/core_spec.rb
@@ -25,4 +25,17 @@ describe "Admin core" do
     
     last_response.should be_ok
   end
+  
+  it "refresh should be ok" do
+    m = Marquee.create(:text => "Foo to the bar.")
+    
+    visit "/"
+    page.has_selector?("ul#marquee li", :text => "Foo to the bar.")
+    
+    put "/admin/marquees/modify", :id => m.id, :marquee => { :text => "Bar to the foo." }
+    post "/admin/refresh"
+    
+    visit "/"
+    page.has_selector?("ul#marquee li", :text => "Bar to the foo.")
+  end
 end
