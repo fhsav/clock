c54fbaef20aa32df77505d2875303f930459721c
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-08-08 16:51:09 -0400

    redid the Gemfile, no more tests.

--- Gemfile
+++ Gemfile
@@ -1,35 +1,36 @@
 source :rubygems
 
-gem 'thin'
-gem 'foreman'
-
 group :test do
   gem 'rack-test', :require => "rack/test"
   gem 'rspec'
-  gem 'capybara', :require => "capybara/dsl"
-  gem 'factory_girl'
 end
+
+gem 'thin'
+
+gem 'foreman'
 
 gem 'rake'
 gem 'sinatra-flash', :require => "sinatra/flash"
 
+gem 'bson_ext', :require => "mongo"
+gem 'mongo_mapper'
+
+gem 'rack-gridfs', :require => "rack/gridfs"
+gem 'joint'
+
+gem 'ohm'
+
+gem 'faye'
+
+gem 'haml'
+
+gem 'sass'
+gem 'compass'
+gem 'susy'
+
+gem 'barista'
+
 gem 'tzinfo'
 gem 'htmlentities'
 
-gem 'bson_ext', :require => "mongo"
-gem 'rack-gridfs', :require => "rack/gridfs"
-gem 'mongo_mapper'
-gem 'joint'
-
-gem 'haml'
-gem 'rdiscount'
-
-gem 'therubyrhino'
-gem 'compass'
-gem 'barista'
-
-gem 'pusher'
-
-gem 'status_cats'
-
-gem 'padrino'
+gem 'padrino', '0.10.7'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,6 @@ require File.expand_path(File.dirname(__FILE__) + "/factories.rb")
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
-  conf.include Capybara::DSL
-  
-  Capybara.app = Padrino.application
 end
 
 def app
@@ -41,29 +38,3 @@ end
 def response
   last_response
 end
-
-def element_visible?(element_id)
-  # does the element exist?
-  exists = page.has_css?(element_id)
-
-  # is the element itself hidden with the .ui-helper-hidden class?
-  self_hidden = page.has_css?("#{element_id}.ui-helper-hidden")
-
-  # is the parent of the element hidden, thus hiding the element?
-  parent_hidden = page.has_css?(".ui-helper-hidden > #{element_id}")
-
-  # is the grandparent of the element, or any other ancestor, hidden?
-  other_ancestor_hidden = page.has_css?(".ui-helper-hidden * #{element_id}")
-
-  # if any of the above conditions are true, then the element is invisible
-  invisible = self_hidden || parent_hidden || other_ancestor_hidden
-
-  # the element is visible if it exists and it is not invisible
-  return (exists && !invisible)
-end
-
-# When writing tests, you use the RSpec method (response). However,
-# when you want to use Capybara, append :js => true to the end of a
-# declaration of the test and use the Capybara method (page). If
-# you're using Capybara, you'll also want to use visit "" instead
-# of get "/" or whatever.
