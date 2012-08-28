25aa9d935d7069af966d167e4afd82c0ecb542ea
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-08-28 19:41:35 -0400

    starting public-facing stuff over. some refresh work.

--- app/controllers/refreshes.rb
+++ app/controllers/refreshes.rb
@@ -4,5 +4,10 @@ Clock.controllers :refreshes do
   end
 
   post :create do
+    client = Faye::Client.new('http://localhost:9292/faye')
+    client.publish('/refreshes', 'timestamp' => Time.now)
+
+    flash[:notice] = "Refreshed!"
+    redirect params[:redirect]
   end
 end
