5ad2873abce4a6a87f5a6bfb288d2c0bae7349a8
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-09-06 22:00:26 -0400

    using heroku addons.

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,16 @@
-require 'yaml'
+require "yaml"
 
-MongoMapper.setup(YAML::load(File.open(File.join(PADRINO_ROOT, '.mongo.yml'))), PADRINO_ENV, :logger => nil)
+if ENV["MONGOHQ_URL"]
+  MongoMapper.config = { PADRINO_ENV => { "uri" => ENV["MONGOHQ_URL"] } }
+  MongoMapper.connect(PADRINO_ENV)
+else
+  MongoMapper.setup(YAML::load(File.open(File.join(PADRINO_ROOT, ".mongo.yml"))), PADRINO_ENV, :logger => nil)
+end
 
-Ohm.connect
+if ENV["REDISTOGO_URL"]
+  redis = URI.parse(ENV["REDISTOGO_URL"])
+
+  Ohm.connect(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  Ohm.connect
+end
