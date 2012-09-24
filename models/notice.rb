6c97d8d7823d678bc71fe5d0174ef4eaba773b6f
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-09-24 14:23:39 -0400

    created model, not spec.

---
+++ models/notice.rb
@@ -0,0 +1,17 @@
+class Notice
+  include MongoMapper::Document
+
+  key :text, String
+  
+  timestamps!
+  
+  validates_presence_of :text
+
+  before_save :format
+
+  private
+
+  def format
+    self.text = self.text.squish
+  end
+end
