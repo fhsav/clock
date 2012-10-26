57e6fce94b03f6dc8ff4ddc248a53c45b0625b02
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-10-26 10:44:05 -0400

    check status of MongoDB and Redis.

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -4,4 +4,16 @@ Clock.controllers :api do
     result[:ms] = (Time.now.to_f * 1000.0).to_i
     result.to_json
   end
+
+  get :ping, :provides => [:json] do
+    result = Hash.new
+
+    if !Schedule.all.blank? && !Redis.get("password").blank?
+      result[:code] = "200"
+    else
+      result[:code] = "503"
+    end
+
+    result.to_json
+  end
 end
