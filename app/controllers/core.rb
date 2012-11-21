a21bf0c336d51b8a698ba4e862467a1e419489c6
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-11-20 20:42:25 -0500

    better controllers.

--- app/controllers/core.rb
+++ app/controllers/core.rb
@@ -1,13 +1,4 @@
 Clock.controllers do
-  before do
-    @schedule = Schedule.activated
-
-    @theme = Theme.activated
-
-    @marquees = Marquee.all
-    @notices = Notice.all
-  end
-
   get :index do
   	if authenticated?
       render :index
@@ -15,14 +6,6 @@ Clock.controllers do
       render 'clock/index', :layout => false
     end
   end
-
-  get :clock do
-    render 'clock/index', :layout => false
-  end
-
-  get :preview, :map => "/clock/preview" do
-    render 'clock/preview', :layout => false
-  end 
 
   get :admin do
   	redirect url(:sessions, :new)
