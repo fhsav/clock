7f0ec54c05cffbba2f8c83196c68e07574a45137
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-01-02 21:47:14 -0500

    fully testing Marquee.

--- spec/unit/models/marquee_spec.rb
+++ spec/unit/models/marquee_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Marquee do
-  let(:marquee) { create(:marquee) }
+  let(:marquee) { create :marquee }
+
+  it { validate_presence_of :text }
 
   it { marquee.should be_valid }
+
+  describe "#format" do
+    it { marquee.text.should_not include("\n") }
+  end
 end
