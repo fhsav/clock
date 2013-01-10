93a1f17fc3910151f87c368778d7f05d26507ffc
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-01-10 17:38:49 -0500

    readded Theme.activated (untested).

--- models/theme.rb
+++ models/theme.rb
@@ -9,4 +9,14 @@ class Theme
   attachment :wallpaper
   
   validates_presence_of :name
+
+  def self.activated
+  	t = self.first(:active => true)
+
+    if t.nil?
+      { :url => "/img/default.jpg", :type => "image/jpeg" }
+    else
+      { :url => url(:themes, :wallpaper, :id => t.id), :type => t.wallpaper.type }
+    end
+  end
 end
