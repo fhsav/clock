ac1b95ccb1352e4a07964d8cccfa49a55eab250f
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-04-27 19:17:53 -0400

    info in html source.

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,10 @@
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development" unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
-CLOCK_VERSION = "0.4.3"
 COMMIT = `git show --pretty=%H`.split[0...1].join(' ')
+BRANCH = `git rev-parse --abbrev-ref HEAD`
+
+CLOCK_VERSION = '0.5.0b'
 
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
