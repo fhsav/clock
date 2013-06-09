3edca357ecbae920db3d37b5ff0d69baa827f585
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-06-08 23:57:36 -0400

    add jammit, plus compass/barista initializers.

--- app/app.rb
+++ app/app.rb
@@ -3,33 +3,28 @@ module Clock
     register Padrino::Cache
     register Padrino::Helpers
     register Padrino::Rendering
-    register Padrino::Sprockets
+    register Jammit
 
     Faye::WebSocket.load_adapter('thin')
 
     use Faye::RackAdapter, :mount => '/faye', :timeout => 25
 
-    asset_paths = [
-      'assets/css',
-      'assets/jsc',
-      'assets/img',
-      'assets/webfonts'
-    ]
-
-    sprockets :url => '_', :minify => (Padrino.env == :production), :paths => asset_paths
-
     enable :sessions
-
-    enable :caching
-
     disable :protection
 
     set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
 
     configure :development do
+      register BaristaInitializer
+      register CompassInitializer
+
       use BetterErrors::Middleware
 
       BetterErrors.application_root = PADRINO_ROOT
+    end
+
+    configure :production do
+      enable :caching
     end
   end
 end
