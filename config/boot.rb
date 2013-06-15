381e680b0888dd3e1eeb9b7bde48f1a1acf0a30c
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-06-15 09:23:43 -0400

    version 0.4.5.

--- config/boot.rb
+++ config/boot.rb
@@ -4,7 +4,7 @@ PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 COMMIT = `git show --pretty=%H`.split[0...1].join(' ')
 BRANCH = `git rev-parse --abbrev-ref HEAD`
 
-CLOCK_VERSION = '0.5.0b'
+CLOCK_VERSION = '0.4.5'
 
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
