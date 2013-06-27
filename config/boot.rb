17be1f240ee47ed72965200d953850cdf07121a3
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-06-26 22:32:25 -0400

    don't include environment, branch, or commit in the source.

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,6 @@
 PADRINO_ENV  = ENV['PADRINO_ENV'] ||= ENV['RACK_ENV'] ||= 'development' unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
-COMMIT = `git show --pretty=%H`.split[0...1].join(' ')
-BRANCH = `git rev-parse --abbrev-ref HEAD`
-
 CLOCK_VERSION = '0.4.5'
 
 require 'rubygems' unless defined?(Gem)
