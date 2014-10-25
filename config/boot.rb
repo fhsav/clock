7028e0986665abeca726ea61a19d1c478a41aa72
gluxon <brandoncheng.personal@gmail.com>
2014-10-25 13:02:10 -0400

    Remove /display check from notices cache and bump version

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 PADRINO_ENV  = ENV['PADRINO_ENV'] ||= ENV['RACK_ENV'] ||= 'development' unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
-CLOCK_VERSION = '0.4.10'
+CLOCK_VERSION = '0.4.11'
 
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
