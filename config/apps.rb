253a15fed5a4033d59730951f96c8df9e256087c
gluxon <brandoncheng.personal@gmail.com>
2015-05-30 12:37:56 -0400

    Mount subapps before main app (Fixes #369)

--- config/apps.rb
+++ config/apps.rb
@@ -4,5 +4,5 @@ Padrino.configure_apps do
   set :session_secret, 'cbebd3f6c9498c542236e90f8cad6ea9a2fa7978d1072a5c78ab2c3695604f1a'
 end
 
-Padrino.mount('Clock::Web', :app_file => Padrino.root('app/app.rb')).to('/')
 Padrino.mount('Clock::API', :app_file => Padrino.root('api/app.rb')).to('/api')
+Padrino.mount('Clock::Web', :app_file => Padrino.root('app/app.rb')).to('/')
